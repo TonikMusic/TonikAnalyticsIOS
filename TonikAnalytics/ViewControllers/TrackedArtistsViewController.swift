9dd9d7fc88c562c0432587ed56522033398567fe
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-11-04 09:37:36 -0800

    Tracked artist colelction View

---
+++ TonikAnalytics/ViewControllers/TrackedArtistsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrackedArtistsViewController.swift
+//  TonikAnalytics
+//
+//  Created by Wesley Espinoza on 11/3/19.
+//  Copyright © 2019 HazeWritesCode. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackedArtistsViewController: UICollectionViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
